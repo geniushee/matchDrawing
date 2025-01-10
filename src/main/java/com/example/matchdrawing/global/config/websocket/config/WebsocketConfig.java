7c353dedb70cb9b317cde5058ff334fb87ca413a
geniushee <h1116jeon@gmail.com>
2025-01-10 12:44:29 +0900

    현재 불필요 의존성 제거

--- src/main/java/com/example/matchdrawing/global/config/websocket/config/WebsocketConfig.java
+++ src/main/java/com/example/matchdrawing/global/config/websocket/config/WebsocketConfig.java
@@ -1,50 +1,50 @@
-package com.example.matchdrawing.global.config.websocket.config;
-
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-@Profile("rabbitmq")
-@Configuration
-@EnableWebSocketMessageBroker
-public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .withSockJS();
-    }
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // 임시 설정
-        registry
-                .setApplicationDestinationPrefixes("/app")
-                .enableStompBrokerRelay("/topic")
-                .setRelayPort(1)
-                .setRelayHost("admin")
-                .setSystemLogin("admin")
-                .setSystemPasscode("admin")
-                .setClientLogin("admin")
-                .setClientPasscode("admin");
-    }
-
-    /**
-     * 통신간 최대 메세지 사이즈, 시간 설정등 할 수 있다. 기본사이즈는 64KB.
-//     */
+//package com.example.matchdrawing.global.config.websocket.config;
+//
+//import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+//import org.springframework.amqp.support.converter.MessageConverter;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+//import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+//import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+//import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+//
+//@Profile("rabbitmq")
+//@Configuration
+//@EnableWebSocketMessageBroker
+//public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
+//
 //    @Override
-//    public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
-//        registry.setMessageSizeLimit(64 * 1024);
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        registry.addEndpoint("/ws")
+//                .withSockJS();
 //    }
-
-    @Bean
-    public MessageConverter messageConverter(){
-        return new Jackson2JsonMessageConverter();
-    }
-}
+//
+//    @Override
+//    public void configureMessageBroker(MessageBrokerRegistry registry) {
+//        // 임시 설정
+//        registry
+//                .setApplicationDestinationPrefixes("/app")
+//                .enableStompBrokerRelay("/topic")
+//                .setRelayPort(1)
+//                .setRelayHost("admin")
+//                .setSystemLogin("admin")
+//                .setSystemPasscode("admin")
+//                .setClientLogin("admin")
+//                .setClientPasscode("admin");
+//    }
+//
+//    /**
+//     * 통신간 최대 메세지 사이즈, 시간 설정등 할 수 있다. 기본사이즈는 64KB.
+////     */
+////    @Override
+////    public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
+////        registry.setMessageSizeLimit(64 * 1024);
+////    }
+//
+//    @Bean
+//    public MessageConverter messageConverter(){
+//        return new Jackson2JsonMessageConverter();
+//    }
+//}
